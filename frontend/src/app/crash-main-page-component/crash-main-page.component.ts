a5c1fa83950fdbd4c9a44cc776632a3749e2e9b2
George Stanculeanu <gstanculeanuoctavian@gmail.com>
2023-05-28 23:20:49 +0200

    wip

---
+++ frontend/src/app/crash-main-page-component/crash-main-page.component.ts
@@ -0,0 +1,7 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-crash-main-page',
+    templateUrl: './crash-main-page.component.html',
+    styleUrls: ['./crash-main-page.component.scss']
+  })
